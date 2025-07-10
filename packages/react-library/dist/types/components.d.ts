6a0c6705ec3eae13acdf2e3313e2b365ff3e13cd
Tufan Butuner <43154254+tufanbutuner@users.noreply.github.com>
2025-07-10 21:20:48 +0100

    initial commit, creation of monorepo and libaries

---
+++ packages/react-library/dist/types/components.d.ts
@@ -0,0 +1,4 @@
+import type { StencilReactComponent } from '@stencil/react-output-target/runtime';
+import { MyComponent as MyComponentElement } from "stencil-library/dist/components/my-component.js";
+export type MyComponentEvents = NonNullable<unknown>;
+export declare const MyComponent: StencilReactComponent<MyComponentElement, MyComponentEvents>;
---
+++ packages/react-library/lib/index.js
@@ -0,0 +1,9 @@
+'use strict';
+export * from './components/stencil-generated/components';
+
+
+function reactLibrary() {
+  return 'Hello from reactLibrary';
+}
+
+export default reactLibrary;
